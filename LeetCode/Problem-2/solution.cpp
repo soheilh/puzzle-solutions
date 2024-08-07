7f060ec3b3c85caeb93f278035500868649c0504
soheil <soheilh105@gmail.com>
2024-08-07 13:10:37 +0330

    Add solution for Problem 2 in LeetCode

---
+++ LeetCode/Problem-2/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* head = new ListNode(0);
+        ListNode* result = head;
+
+        while(l1 != nullptr || l2 != nullptr){
+            int val1 = (l1 == nullptr) ? 0 : l1->val;
+            int val2 = (l2 == nullptr) ? 0 : l2->val;
+            int sum = val1 + val2 + carry;
+            carry = 0;
+            if(sum > 9){
+                sum = sum - 10;
+                carry = 1;
+            }
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
+            result->next = new ListNode(sum);
+            result = result->next;
+        }
+        if (carry == 1) {
+            result->next = new ListNode(carry);
+        }
+
+        return head->next;
+    }
+};
